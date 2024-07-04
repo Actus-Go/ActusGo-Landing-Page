2041666d8dd9337e64ed9f19011ae2f230f2e2b3
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-07-04 17:00:47 +0300

    chore: Add configuration files and dependencies for Next.js and Tailwind CSS

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Actus Go",
+  description: "Empower local businesses to thrive in a global market. By gamifying the shopping experience, we’re rewriting the rules.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
