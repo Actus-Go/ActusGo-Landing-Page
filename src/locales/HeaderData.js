6b553f85209761818ac3c331553b355edc9eb69b
HazemMo <hazemmohamed345674@gmail.com>
2024-10-30 15:39:58 +0300

    update the image hero

--- src/locales/HeaderData.js
+++ src/locales/HeaderData.js
@@ -4,12 +4,12 @@ const headerData = {
       description:
         "Join our adventure trips and discover breathtaking destinations. Experience the thrill of a lifetime!",
       images: [
-        { src: "/images/3d-casual-life-credit-cards-1.png", alt: "Image 1" },
-        { src: "/images/3d-kit-stack-of-bills.png", alt: "Image 2" },
-        { src: "/images/3d-glassy-floating-pie-chart-and-coins.png", alt: "Image 3" },
-        { src: "/images/3d-kit-coin.png", alt: "Image 4" },
-        { src: "/images/3d-casual-life-mobile-banking-cashback.png", alt: "Image 5" },
-        { src: "/images/3d-kit-coin.png", alt: "Image 6" },
+        { src: "/images/dynamic-sale-sign-pink.png", alt: "Image 1" },
+        { src: "/images/dynamic-search-window.png", alt: "Image 2" },
+        { src: "/images/dynamic-rectangle-yellow-1.png", alt: "Image 3" },
+        { src: "/images/dynamic-price-tag-on-rectangle-pink.png", alt: "Image 4" },
+        { src: "/images/dynamic-arrow (1).png", alt: "Image 5" },
+        // { src: "/images/dynamic-price-tag-on-rectangle-pink.png", alt: "Image 6" },
       ],
     },
     ar: {
@@ -17,12 +17,12 @@ const headerData = {
       description:
         "انضم إلى رحلات المغامرة لدينا واستكشف وجهات خلابة. عش تجربة لا تنسى!",
       images: [
-        { src: "/images/3d-casual-life-credit-cards-1.png", alt: "الصورة 1" },
-        { src: "/images/3d-kit-stack-of-bills.png", alt: "الصورة 2" },
-        { src:  "/images/3d-glassy-floating-pie-chart-and-coins.png", alt: "الصورة 3" },
-        { src: "/images/3d-kit-coin.png", alt: "الصورة 4" },
-        { src: "/images/3d-casual-life-mobile-banking-cashback.png", alt: "الصورة 5" },
-        { src: "/images/3d-kit-coin.png", alt: "الصورة 6" },
+        { src: "/images/dynamic-sale-sign-pink.png", alt: "الصورة 1" },
+        { src: "/images/dynamic-search-window.png", alt: "الصورة 2" },
+        { src:  "/images/dynamic-rectangle-yellow-1.png", alt: "الصورة 3" },
+        { src: "/images/dynamic-price-tag-on-rectangle-pink.png", alt: "الصورة 4" },
+        { src: "/images/dynamic-arrow (1).png", alt: "الصورة 5" },
+        // { src: "/images/dynamic-instagram-logo.png", alt: "الصورة 6" },
       ],
     },
   };
